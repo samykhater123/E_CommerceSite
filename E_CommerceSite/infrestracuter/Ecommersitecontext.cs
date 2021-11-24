3ee6ee1de30eac8cfb4ef2beb9e10f538e0f541c
samy khater <samykhater55@outlook.com>
2021-11-24 18:48:28 +0200

    Add project files.

---
+++ E_CommerceSite/infrestracuter/Ecommersitecontext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_CommerceSite.infrestracuter
+{
+    public class Ecommersitecontext:DbContext
+    {
+        public Ecommersitecontext(DbContextOptions<Ecommersitecontext>options)
+            :base(options)
+        {
+
+        }
+    }
+}
